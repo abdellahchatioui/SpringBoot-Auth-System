fc478d29b8028742133ca9d1f105289e2acb5933
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-22 23:23:16 +0100

    initial commit

---
+++ src/main/java/com/example/sb_auth_system/SbAuthSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb_auth_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAuthSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbAuthSystemApplication.class, args);
+    }
+
+}
