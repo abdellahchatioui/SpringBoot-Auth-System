32b8a2086a287ce6132e5e6ad0d2a5a00350ac37
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-25 23:03:10 +0100

    fix: fic the Users importation

--- src/main/java/com/example/sb_auth_system/repository/UserRepository.java
+++ src/main/java/com/example/sb_auth_system/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.example.sb_auth_system.repository;
 
-import com.example.sb_auth_system.entity.User;
+import com.example.sb_auth_system.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByEmail(String email);
+public interface UserRepository extends JpaRepository<Users,Integer> {
+    Users findByEmail(String email);
 }
