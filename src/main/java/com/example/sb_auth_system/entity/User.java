445a4ecc47156bdc73de1bdf1d36cd8c70f0ff8b
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-25 22:46:00 +0100

    feat: create User entity with email, password, and role fields

--- src/main/java/com/example/sb_auth_system/entity/User.java
+++ src/main/java/com/example/sb_auth_system/entity/User.java
@@ -1,4 +1,69 @@
 package com.example.sb_auth_system.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @JsonIgnore
+    @Size(message = "min 4 word" , min = 4)
+    @Column(nullable = false)
+    private  String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User(Integer id, String email, String password, Role role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
 }
