a952fffb8498356533c4739dfbd1a72519a70fdc
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-26 23:18:05 +0100

    feat: implement CustomUserDetails to adapt Users entity for Spring Security

---
+++ src/main/java/com/example/sb_auth_system/security/CustomUserDetails.java
@@ -0,0 +1,40 @@
+package com.example.sb_auth_system.security;
+
+import com.example.sb_auth_system.entity.Role;
+import com.example.sb_auth_system.entity.Users;
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class CustomUserDetails implements UserDetails {
+
+    private Users users;
+
+    public CustomUserDetails(Users users) {
+        this.users = users;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(users.getRole()));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return users.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return users.getEmail();
+    }
+}
