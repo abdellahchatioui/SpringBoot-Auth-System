d717ba64cf743f1c82c8e13ede4f3375b53ec450
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-27 21:01:50 +0100

    feat: add register endpoint in AuthController

--- src/main/java/com/example/sb_auth_system/controller/AuthController.java
+++ src/main/java/com/example/sb_auth_system/controller/AuthController.java
@@ -1,4 +1,26 @@
 package com.example.sb_auth_system.controller;
 
+import com.example.sb_auth_system.entity.Users;
+import com.example.sb_auth_system.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    @Autowired
+    AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Users> register(@RequestBody Users user){
+        return  ResponseEntity.ok(authService.register(user));
+    }
+
 }
