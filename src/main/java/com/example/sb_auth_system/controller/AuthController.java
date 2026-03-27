7e3218f6bb6824341c75596bcfc7d0e08d9a3248
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-27 21:04:37 +0100

    refactor: replace @RestController with @Controller

--- src/main/java/com/example/sb_auth_system/controller/AuthController.java
+++ src/main/java/com/example/sb_auth_system/controller/AuthController.java
@@ -4,14 +4,13 @@ import com.example.sb_auth_system.entity.Users;
 import com.example.sb_auth_system.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
