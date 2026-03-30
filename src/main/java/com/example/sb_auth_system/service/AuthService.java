7f27bd5e38688b66825d72db5af5fc6204792977
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-30 21:59:04 +0100

    feat: implement JWT token generation

--- src/main/java/com/example/sb_auth_system/service/AuthService.java
+++ src/main/java/com/example/sb_auth_system/service/AuthService.java
@@ -3,6 +3,7 @@ package com.example.sb_auth_system.service;
 import com.example.sb_auth_system.entity.Role;
 import com.example.sb_auth_system.entity.Users;
 import com.example.sb_auth_system.repository.UserRepository;
+import com.example.sb_auth_system.security.JwtService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,11 +17,13 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepos;
+    private final JwtService jwtService;
 
-    public AuthService(PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, UserRepository userRepos) {
+    public AuthService(PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, UserRepository userRepos, JwtService jwtService) {
         this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
         this.userRepos = userRepos;
+        this.jwtService = jwtService;
     }
 
     public String login(Users user){
@@ -32,7 +35,7 @@ public class AuthService {
                 )
         );
 
-        return "LOGIN SUCCESS";
+        return jwtService.generateToken(user);
     }
 
     public Users register(Users user){
