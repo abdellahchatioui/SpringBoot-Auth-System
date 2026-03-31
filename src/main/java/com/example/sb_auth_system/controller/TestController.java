38239fedfb71f5e85875a3520eed440077b16abd
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-31 23:38:08 +0100

    feat: implemet TestController for testing

--- src/main/java/com/example/sb_auth_system/controller/TestController.java
+++ src/main/java/com/example/sb_auth_system/controller/TestController.java
@@ -1,4 +1,22 @@
 package com.example.sb_auth_system.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/users")
+    public String users(){
+        return "SECURED DATA";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "WORKING";
+    }
+
 }
