a5c89df6f464dbcc86176a5cd6cefe2f5f566e81
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-04-08 21:24:42 +0100

    feat: add existsByToken method to TokenBlacklistRepository

--- src/main/java/com/example/sb_auth_system/repository/TokenBlacklistRepository.java
+++ src/main/java/com/example/sb_auth_system/repository/TokenBlacklistRepository.java
@@ -1,4 +1,14 @@
 package com.example.sb_auth_system.repository;
 
-public interface TokenBlacklistRepository {
+import com.example.sb_auth_system.entity.TokenBlacklist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TokenBlacklistRepository extends JpaRepository<TokenBlacklist,Long> {
+
+    Optional<TokenBlacklist> findByToken(String token);
+
+    boolean existsByToken(String token);
+
 }
