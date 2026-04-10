eadf3128162f3d4edcb3767a8045372d0ae4445c
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-04-10 22:51:29 +0100

    refactor: use Redis blacklist in JwtAuthenticationFilter

--- src/main/java/com/example/sb_auth_system/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/sb_auth_system/security/JwtAuthenticationFilter.java
@@ -3,10 +3,12 @@ package com.example.sb_auth_system.security;
 import com.example.sb_auth_system.entity.Users;
 import com.example.sb_auth_system.repository.TokenBlacklistRepository;
 import com.example.sb_auth_system.repository.UserRepository;
+import com.example.sb_auth_system.service.TokenBlacklistService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,6 +22,9 @@ import java.util.List;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    TokenBlacklistService tokenBlacklistService;
+
     private final UserRepository userRepos;
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
@@ -45,7 +50,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         JWTtoken = authHeader.substring(7);
 
-        if(tokenBlacklistRepository.existsByToken(JWTtoken)){
+        if (tokenBlacklistService.isBlacklisted(JWTtoken)) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
