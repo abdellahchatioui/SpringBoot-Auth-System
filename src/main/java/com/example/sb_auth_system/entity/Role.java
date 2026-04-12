d982d03b52bb80eda7905f10b97d8061b39ec2c2
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-04-12 22:58:41 +0100

    feat: assign permissions to roles

--- src/main/java/com/example/sb_auth_system/entity/Role.java
+++ src/main/java/com/example/sb_auth_system/entity/Role.java
@@ -1,6 +1,26 @@
 package com.example.sb_auth_system.entity;
 
+import java.util.Set;
+
 public enum Role {
-    USER,
-    ADMIN
+    USER(Set.of(
+            Permission.READ_USER
+    )),
+
+
+    ADMIN(Set.of(
+            Permission.READ_USER,
+            Permission.CREATE_USER,
+            Permission.DELETE_USER
+    ));
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
 }
